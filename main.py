0ef918b051147abe845b848f1b934153df6e23cb
Pierre-Henri Dezanneau <ph.dezanneau@gmail.com>
2024-06-09 10:06:42 +0200

    Add retropie

--- main.py
+++ main.py
@@ -21,14 +21,17 @@ def main():
     pub_p5p77r = MqttPub(monitor, args.mqtt_hostname, args.mqtt_port, "sysinfo/probe_P5P77R/SENSOR", 2)
     pub_n5lth1 = MqttPub(monitor, args.mqtt_hostname, args.mqtt_port, "sysinfo/probe_N5LTH1/SENSOR", 2)
     pub_e9cd9k = MqttPub(monitor, args.mqtt_hostname, args.mqtt_port, "sysinfo/probe_E9CD9K/SENSOR", 2)
+    pub_x2h7dr = MqttPub(monitor, args.mqtt_hostname, args.mqtt_port, "sysinfo/probe_X2H7DR/SENSOR", 2)
+
     pub_bw9q48.start()
     pub_ki8q49.start()
     pub_p5p77r.start()
     pub_n5lth1.start()
     pub_e9cd9k.start()
+    pub_x2h7dr.start()
 
     logging.info("Mqtt publishers are now ready")
-    bme280_probe = Bme280Probe(args, pub_bw9q48, pub_ki8q49, pub_p5p77r, pub_n5lth1, pub_e9cd9k)
+    bme280_probe = Bme280Probe(args, pub_bw9q48, pub_ki8q49, pub_p5p77r, pub_n5lth1, pub_e9cd9k, pub_x2h7dr)
     monitor.register_client(bme280_probe)
     bme280_probe.start_reading()
 
